961644e4e5a7fabb92bea81dc4aa307a57c8a220
ajaycalicut17 <ajaycalicut17@gmail.com>
2023-08-05 22:47:02 +0530

    password cast removed

--- tests/Models/User.php
+++ tests/Models/User.php
@@ -43,7 +43,6 @@ class User extends Authenticatable
      */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
 
     protected $dispatchesEvents = [
